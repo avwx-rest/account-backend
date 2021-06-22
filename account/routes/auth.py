aa27067c0f4221e8d8769b050de3ba1261f027b7
Michael duPont <flyinactor91@gmail.com>
2021-06-22 00:27:37 -0400

    Add refresh endpoint

--- account/routes/auth.py
+++ account/routes/auth.py
@@ -2,14 +2,33 @@
 Authentication router
 """
 
+# pylint: disable=too-few-public-methods
+
+from datetime import timedelta
+
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi_jwt_auth import AuthJWT
+from pydantic import BaseModel
 
 from account.models.user import User, UserAuth
 from account.util.password import hash_password
 
 
 router = APIRouter()
+
+
+class AccessToken(BaseModel):
+    """Access token details"""
+
+    access_token: str
+    access_token_expires: timedelta = timedelta(minutes=15)
+
+
+class RefreshToken(AccessToken):
+    """Access and refresh token details"""
+
+    refresh_token: str
+    refresh_token_expires: timedelta = timedelta(days=30)
 
 
 @router.post("/login")
@@ -19,4 +38,13 @@ async def login(user_auth: UserAuth, auth: AuthJWT = Depends()):
     if not user or hash_password(user_auth.password) != user.password:
         raise HTTPException(status_code=401, detail="Bad username or password")
     access_token = auth.create_access_token(subject=user.email)
-    return {"access_token": access_token}
+    refresh_token = auth.create_refresh_token(subject=user.email)
+    return RefreshToken(access_token=access_token, refresh_token=refresh_token)
+
+
+@router.post("/refresh")
+async def refresh(auth: AuthJWT = Depends()):
+    """Returns a new access token from a refresh token"""
+    auth.jwt_refresh_token_required()
+    access_token = auth.create_access_token(subject=auth.get_jwt_subject())
+    return AccessToken(access_token=access_token)
