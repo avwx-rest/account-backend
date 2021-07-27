e7aabb75cbf419d53635718c2f09cf883c11fb0f
Michael duPont <flyinactor91@gmail.com>
2021-07-27 02:29:18 -0400

    Add plan tests

--- tests/data.py
+++ tests/data.py
@@ -6,6 +6,7 @@ import asyncio as aio
 import json
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
+from typing import Optional
 
 from account.models.plan import Plan
 from account.models.token import TokenUsage
@@ -17,31 +18,34 @@ DATA = Path(__file__).parent / "data"
 PLANS = json.load(DATA.joinpath("plans.json").open())
 
 
-async def add_empty_user() -> str:
-    """Adds minimal user to user collection"""
-    email = "empty@test.io"
+def make_user(email: str, offset: Optional[int] = 0) -> User:
+    """Returns a minimal, uncommitted User"""
+    now = None
+    if offset is not None:
+        now = datetime.now(tz=timezone.utc) - timedelta(days=offset)
     user = User(
         email=email,
         password=hash_password(email),
-        email_confirmed_at=datetime.now(tz=timezone.utc),
+        email_confirmed_at=now,
     )
+    return user
+
+
+async def add_empty_user() -> str:
+    """Adds minimal user to user collection"""
+    user = make_user("empty@test.io")
     await user.create()
-    return email
+    return user.email
 
 
 async def add_token_user(history: bool = False) -> str:
     """Add user with an app token to user collection"""
+    user = make_user("token@test.io", offset=7)
     token = await UserToken.new()
-    email = "token@test.io"
-    now = datetime.now(tz=timezone.utc)
-    user = User(
-        email=email,
-        password=hash_password(email),
-        email_confirmed_at=now - timedelta(days=7),
-        tokens=[token],
-    )
+    user.tokens = [token]
     await user.create()
     if history:
+        now = datetime.now(tz=timezone.utc)
         tasks = []
         for i in range(3):
             day = now - timedelta(days=i)
@@ -54,7 +58,15 @@ async def add_token_user(history: bool = False) -> str:
             )
             tasks.append(usage.create())
         await aio.gather(*tasks)
-    return email
+    return user.email
+
+
+async def add_plan_user(plan: str = "free") -> str:
+    """Add user with a specific plan to user collection"""
+    user = make_user(f"plan-{plan}@test.io")
+    user.plan = await Plan.by_key(plan)
+    await user.create()
+    return user.email
 
 
 async def add_plans(*keys: str) -> None:
---
+++ tests/views/test_plan.py
@@ -0,0 +1,33 @@
+"""
+Plan information tests
+"""
+
+import pytest
+from httpx import AsyncClient
+
+from tests.data import add_plan_user, add_plans
+from tests.util import auth_headers
+
+
+@pytest.mark.asyncio
+async def test_get_plan(client: AsyncClient) -> None:
+    """Test plan endpoint returns free plan"""
+    key = "free"
+    await add_plans(key)
+    email = await add_plan_user(key)
+    auth = await auth_headers(client, email)
+    resp = await client.get("/plan", headers=auth)
+    assert resp.status_code == 200
+    plan = resp.json()
+    assert plan["key"] == key
+
+
+@pytest.mark.asyncio
+async def test_get_all_plans(client: AsyncClient) -> None:
+    """Test public plan list endpoint"""
+    keys = ("free",)
+    await add_plans(*keys)
+    resp = await client.get("plan/all")
+    assert resp.status_code == 200
+    plans = resp.json()
+    assert len(plans) == len(keys)
