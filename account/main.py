a9a5733d7c6848c8d0bc531f853d57c389d927db
Michael duPont <flyinactor91@gmail.com>
2021-08-23 01:45:36 -0400

    Enable stripe fulfillment router

--- account/main.py
+++ account/main.py
@@ -11,6 +11,7 @@ from account.routes.mail import router as MailRouter
 from account.routes.notification import router as NotificationRouter
 from account.routes.plan import router as PlanRouter
 from account.routes.register import router as RegisterRouter
+from account.routes.stripe import router as StripeRouter
 from account.routes.token import router as TokenRouter
 from account.routes.user import router as UserRouter
 
@@ -20,6 +21,7 @@ app.include_router(MailRouter)
 app.include_router(NotificationRouter)
 app.include_router(PlanRouter)
 app.include_router(RegisterRouter)
+app.include_router(StripeRouter)
 app.include_router(TokenRouter)
 app.include_router(UserRouter)
 
